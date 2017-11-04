e4bed4d69c975385b366d7381683dac9452ff00f
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2017-11-04 19:08:02 -0400

    Minor Changes

---
+++ Matchima/Matchima/DefaultsKeysAndValues.h
@@ -0,0 +1,48 @@
+//
+//  DefaultsKeysAndValues.h
+//  Matchima
+//
+//  Created by Mukhtar Yusuf on 9/28/17.
+//  Copyright © 2017 Mukhtar Yusuf. All rights reserved.
+//
+
+#ifndef DefaultsKeysAndValues_h
+#define DefaultsKeysAndValues_h
+
+//----Keys----
+
+//--User Defaults Keys--
+#define APP_LAUNCHED_ONCE @"App launched once"
+#define WOOD_THEME @"Wood theme"
+#define DARK_THEME @"Dark theme"
+#define CLASSIC_THEME @"Classic theme"
+
+#define SETTINGS @"Settings"
+#define WOOD_THEME_SETTINGS @"Wood theme settings"
+#define DARK_THEME_SETTINGS @"Dark theme settings"
+#define CLASSIC_THEME_SETTINGS @"Classic theme settings"
+
+//--Settings Keys--
+#define HAS_BACKGROUND_IMAGE @"Has background image"
+#define BACKGROUND_IMAGE @"Background image"
+#define CARDBACK_IMAGE @"Cardback image"
+#define MENU_COLOR @"Menu Color"
+
+//--Menu Keys--
+#define RED @"Red"
+#define GREEN @"Green"
+#define BLUE @"Blue"
+#define ALPHA @"Alpha"
+
+//----Values----
+
+//--Background Values--
+#define WOOD_BACKGROUND @"Wood background 2"
+#define DARK_BACKGROUND @"Wood background 3"
+#define NO_BACKGROUND @"No background"
+
+//--Cardback Values--
+#define CARDBACK @"cardback"
+#define CARDBACK_DARK @"cardback3"
+
+#endif /* DefaultsKeysAndValues_h */
