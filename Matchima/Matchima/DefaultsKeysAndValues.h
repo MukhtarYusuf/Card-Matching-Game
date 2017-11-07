b8c97f107f35dd5a95b9a24fd44b734879cb51f3
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2017-11-07 03:30:47 -0500

    Added more Key Value Constants to Header

--- Matchima/Matchima/DefaultsKeysAndValues.h
+++ Matchima/Matchima/DefaultsKeysAndValues.h
@@ -22,17 +22,30 @@
 #define DARK_THEME_SETTINGS @"Dark theme settings"
 #define CLASSIC_THEME_SETTINGS @"Classic theme settings"
 
+#define GAME_STATE @"Game State"
+
 //--Settings Keys--
 #define HAS_BACKGROUND_IMAGE @"Has background image"
 #define BACKGROUND_IMAGE @"Background image"
 #define CARDBACK_IMAGE @"Cardback image"
 #define MENU_COLOR @"Menu Color"
+#define SAVE_BUTTON_COLOR @"Save Button Color"
 
 //--Menu Keys--
 #define RED @"Red"
 #define GREEN @"Green"
 #define BLUE @"Blue"
 #define ALPHA @"Alpha"
+
+//--Game State Keys--
+#define IS_GAME_ACTIVE @"Is game active"
+#define IS_GAME_PAUSED @"Is game paused"
+#define SCORE @"Score"
+#define MULTIPLIER @"Multiplier"
+#define TOTAL_PLAY_TIME @"Total Play Time"
+#define TOTAL_TIME @"Total time"
+#define SUB_TIME @"Sub time"
+#define CARDS @"Cards"
 
 //----Values----
 
@@ -44,5 +57,31 @@
 //--Cardback Values--
 #define CARDBACK @"cardback"
 #define CARDBACK_DARK @"cardback3"
+
+//--Color Values--
+
+//-Wood Theme Menu Color Values-
+#define RED_WOOD_MENU_VALUE 118
+#define GREEN_WOOD_MENU_VALUE 67
+#define BLUE_WOOD_MENU_VALUE 0
+#define ALPHA_WOOD_MENU_VALUE 0.35
+
+//-Classic Theme Menu Color Values-
+#define RED_CLASSIC_MENU_VALUE 11
+#define GREEN_CLASSIC_MENU_VALUE 70
+#define BLUE_CLASSIC_MENU_VALUE 0
+#define ALPHA_CLASSIC_MENU_VALUE 0.35
+
+//-Wood Theme Save Button Color Values-
+#define RED_WOOD_SAVE_BUTTON_VALUE 118
+#define GREEN_WOOD_SAVE_BUTTON_VALUE 67
+#define BLUE_WOOD_SAVE_BUTTON_VALUE 0
+#define ALPHA_WOOD_SAVE_BUTTON_VALUE 0.60
+
+//-Classic Theme Save Button Color Values-
+#define RED_CLASSIC_SAVE_BUTTON_VALUE 11
+#define GREEN_CLASSIC_SAVE_BUTTON_VALUE 40
+#define BLUE_CLASSIC_SAVE_BUTTON_VALAUE 0
+#define ALPHA_CLASSIC_SAVE_BUTTON_VALUE 0.60
 
 #endif /* DefaultsKeysAndValues_h */
